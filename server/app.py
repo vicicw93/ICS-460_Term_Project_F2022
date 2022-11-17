5dc715c0e0449407df742ff89cc66ca309566680
alexsimpson440dev <alexsimpson440dev@gmail.com>
2022-11-16 19:29:21 -0600

    updated app for new console

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/display_stock", methods=['POST'])
 def devices():
     request_json = request.json
-    stock_ticker = request_json['intent']['params']['ticker']['original']
+    stock_ticker = request_json['intent']['params']['company']['original'].split(' ')[-1]
     print(stock_ticker)
     response = {
 
